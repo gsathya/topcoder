377ece5fc14317eda3087f364721f7e087f89a38
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-08-10 11:54:21 -0700

    add more SRMs

---
+++ SRM 256/GridGenerator.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+import math,string,itertools,fractions,heapq,collections,re,array,bisect
+
+class GridGenerator:
+    def generate(self, row, col):
+        table = []
+
+        for i in range(len(row)):
+            table.append([])
+            for j in range(len(col)):
+                table[i].append(0)
+            
+        for i in range(len(row)):
+            for j in range(len(col)):
+                if j == 0:
+                    table[i][j] = col[i]
+                elif i == 0:
+                    table[i][j] = row[j]
+                else:
+                    table[i][j] = table[i-1][j] + table[i][j-1] + table[i-1][j-1]
+
+        return table[i][j]
+
+# CUT begin
+# TEST CODE FOR PYTHON {{{
+import sys, time, math
+
+def tc_equal(expected, received):
+    try:
+        _t = type(expected)
+        received = _t(received)
+        if _t == list or _t == tuple:
+            if len(expected) != len(received): return False
+            return all(tc_equal(e, r) for (e, r) in zip(expected, received))
+        elif _t == float:
+            eps = 1e-9
+            d = abs(received - expected)
+            return not math.isnan(received) and not math.isnan(expected) and d <= eps * max(1.0, abs(expected))
+        else:
+            return expected == received
+    except:
+        return False
+
+def pretty_str(x):
+    if type(x) == str:
+        return '"%s"' % x
+    elif type(x) == tuple:
+        return '(%s)' % (','.join( (pretty_str(y) for y in x) ) )
+    else:
+        return str(x)
+
+def do_test(row, col, __expected):
+    startTime = time.time()
+    instance = GridGenerator()
+    exception = None
+    try:
+        __result = instance.generate(row, col);
+    except:
+        import traceback
+        exception = traceback.format_exc()
+    elapsed = time.time() - startTime   # in sec
+
+    if exception is not None:
+        sys.stdout.write("RUNTIME ERROR: \n")
+        sys.stdout.write(exception + "\n")
+        return 0
+
+    if tc_equal(__expected, __result):
+        sys.stdout.write("PASSED! " + ("(%.3f seconds)" % elapsed) + "\n")
+        return 1
+    else:
+        sys.stdout.write("FAILED! " + ("(%.3f seconds)" % elapsed) + "\n")
+        sys.stdout.write("           Expected: " + pretty_str(__expected) + "\n")
+        sys.stdout.write("           Received: " + pretty_str(__result) + "\n")
+        return 0
+
+def run_tests():
+    sys.stdout.write("GridGenerator (250 Points)\n\n")
+
+    passed = cases = 0
+    case_set = set()
+    for arg in sys.argv[1:]:
+        case_set.add(int(arg))
+
+    with open("GridGenerator.sample", "r") as f:
+        while True:
+            label = f.readline()
+            if not label.startswith("--"): break
+
+            row = []
+            for i in range(0, int(f.readline())):
+                row.append(int(f.readline().rstrip()))
+            row = tuple(row)
+            col = []
+            for i in range(0, int(f.readline())):
+                col.append(int(f.readline().rstrip()))
+            col = tuple(col)
+            f.readline()
+            __answer = int(f.readline().rstrip())
+
+            cases += 1
+            if len(case_set) > 0 and (cases - 1) in case_set: continue
+            sys.stdout.write("  Testcase #%d ... " % (cases - 1))
+            passed += do_test(row, col, __answer)
+
+    sys.stdout.write("\nPassed : %d / %d cases\n" % (passed, cases))
+
+    T = time.time() - 1407694573
+    PT, TT = (T / 60.0, 75.0)
+    points = 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))
+    sys.stdout.write("Time   : %d minutes %d secs\n" % (int(T/60), T%60))
+    sys.stdout.write("Score  : %.2f points\n" % points)
+
+if __name__ == '__main__':
+    run_tests()
+
+# }}}
+# CUT end
